c112ab5a99f736a590bf8367168c4932659da800
James Tew <145987450+JTew27@users.noreply.github.com>
2024-12-11 19:37:17 +0000

    Update NumberSorter.java with comments

--- Week7_Lists_Stacks_Queues/src/NumberSorter.java
+++ Week7_Lists_Stacks_Queues/src/NumberSorter.java
@@ -13,17 +13,17 @@ public class NumberSorter {
 			srcStack.push(number);
 		
 		while(!srcStack.isEmpty()) {
-			int next = srcStack.pop();
+			int next = srcStack.pop(); //pop next item off top of list
 			System.out.println(next);
 			while(destStack.size() < 0){
 				srcStack.push(destStack.pop());	
 	   
 				}
-			destStack.push(next);
+			destStack.push(next); //push next item onto stack
 			//System.out.println(srcStack);
 			System.out.println(destStack);
 			}
-		for (int number : destStack)
+		for (int number : destStack) //for numbers in the stack that is pushed too
 	     	System.out.println(number + " ");
 		}
 		//System.out.println();	
